e40a5b5f50ce0b1cdeb90e9d7ab72dd24fc02c0d
jessfraser8 <jfraser4@usf.edu>
2024-05-21 19:58:11 -0400

    Create EV class extending Car with additional charge property.

---
+++ cc_4.js
@@ -0,0 +1,31 @@
+//U09226075
+//Create Car class.
+class Car {
+    constructor(make, speed){
+        this.make = make;
+        this.speed = speed;
+        console.log(`The car is a ${make} driving ${speed} km/h.`);
+    }
+
+//Add acceleration method.
+    accelerate(){
+        this.speed = this.speed + 10;
+        console.log(`The new speed is ${this.speed} km/h.`);
+        return this.speed + 10;
+    }
+
+//Add brake method.
+    brake(){
+        this.speed = this.speed - 5;
+        console.log(`The new speed is ${this.speed} km/h.`);
+        return this.speed;
+    }
+}
+
+//Create EV class.
+class EV extends Car {
+    constructor(make, speed, charge){
+        super(make, speed);
+        this.charge = charge;
+    }
+}
